1924d0e87fe5ff32f9fa8373765829bd62169822
exloit <lf741258@gmail.com>
2020-05-12 00:58:12 +0800

    20200511

--- controllers/subdomains.go
+++ controllers/subdomains.go
@@ -1,11 +1,29 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"fmt"
+	"github.com/astaxie/beego"
+	"time"
+)
+
 
 type SubController struct {
 	beego.Controller
 }
 
 func (c *SubController)  Get(){
+	c.TplName = "subdomains.html"
+}
 
+func (c *SubController) Post(){
+	domain := c.GetString("domains")
+	resp := &JSONStruct{200, "test "+ domain}
+	go StartSubDomainsScan(domain)
+	c.Data["json"] = &resp
+	c.ServeJSON()
+}
+
+func StartSubDomainsScan(target string){
+	time.Sleep(1000)
+	fmt.Println(target)
 }
